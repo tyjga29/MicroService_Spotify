a9faf98a5f3ec50829b469edf8c0195faced5467
Tobias Kapp <kapp.tob@gmail.com>
2023-11-02 09:57:53 +0100

    Spotify done

--- spotify_controller.py
+++ spotify_controller.py
@@ -1,23 +1,10 @@
-import schedule
-import asyncio
-
-from spotify_metadata import Spotify_Metadata
 from spotify_api_requests import play_music
 from spotify_tokens_api import get_access_token
 from spotify_tokens_api import check_access_token
 
-async def run_check_viable_access_token():
-    while True:
-        await asyncio.to_thread(check_access_token)
-        await asyncio.sleep(60)  # Run the check every minute
+def spotify_play_music(spotify_uri):
+    check_access_token()
+    access_token = get_access_token()
+    play_music(access_token, spotify_uri)
 
-async def main():
-    play_music(get_access_token(), "spotify:playlist:37i9dQZF1DX6J5NfMJS675")
-    
-    # Schedule other tasks if needed
-
-    # Start the check_viable_access_token task
-    await asyncio.create_task(run_check_viable_access_token())
-
-if __name__ == '__main__':
-    asyncio.run(main())
+spotify_play_music("spotify:playlist:37i9dQZF1DX6J5NfMJS675")
